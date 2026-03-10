32c492cb79a106785b1651fa11c1d85a9f5a6386
Peterson Lima <peter.lima104dev@hotmail.com>
2026-03-10 10:53:41 -0300

    Adicionando instalação pwa

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,25 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open("pl-financas")
-      .then((cache) =>
-        cache.addAll(["./", "./index.html", "./style.css", "./app.js"]),
-      ),
+const CACHE_NAME = "monely-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
+  );
 });
