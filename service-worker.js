12bd8d299c9171dd2990430dce64737842430562
Peterson Lima <peter.lima104dev@hotmail.com>
2026-03-10 18:16:57 -0300

    Corrigindo bug para instalação do app

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,45 @@
-const CACHE_NAME = "monely-cache-v1";
-const urlsToCache = [
+const CACHE_NAME = "monely-v1";
+const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "/manifest.json",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css",
 ];
 
+// INSTALAÇÃO
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    }),
   );
+  self.skipWaiting();
 });
 
+// ATIVAÇÃO
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        }),
+      ),
+    ),
+  );
+  self.clients.claim();
+});
+
+// FETCH (offline first)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request)),
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
   );
 });
