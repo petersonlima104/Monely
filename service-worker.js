edb59b8404e15f2f48b6aadcdde63f46e6a88d89
Peterson Lima <peter.lima104dev@hotmail.com>
2026-03-11 09:05:55 -0300

    Corrigindo bug de instalação pwa

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,6 @@ const FILES_TO_CACHE = [
   "/manifest.json",
   "/assets/icon-192.png",
   "/assets/icon-512.png",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css",
 ];
 
 // INSTALAÇÃO
